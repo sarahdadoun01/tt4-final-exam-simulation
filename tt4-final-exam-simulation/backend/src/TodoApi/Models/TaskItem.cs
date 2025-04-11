13d29defcc265836034b03e716da13f596b5851f
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-04-11 16:52:37 -0400

    Add .gitignore and clean ignored files

--- tt4-final-exam-simulation/backend/src/TodoApi/Models/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoApi.Models{
-    public class TaskItem{
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool Completed { get; set; }
-    }
-}
-
