8b6a6bb13aefe1ba9e51b37b0d841fd9bf962c1e
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-04-11 16:42:41 -0400

    modified structure

--- tt4-final-exam-simulation/backend/src/TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ tt4-final-exam-simulation/backend/src/TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634b29559fdff8a5d9f38fa963f17b46a9942f3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc545d9f5863a759806cc5cecd0dac4f8d9ae1f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
