5fa5ab12377a3ef06f3ff1eb430f57d7391f279f
sarahdadoun01 <85398444+sarahdadoun@users.noreply.github.com>
2025-04-11 16:44:52 -0400

    renamed

---
+++ final-project/backend/src/TodoApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(options => options.UseSqlite("Data Source=tasks.db"));
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowAll",
+            policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+        );
+    }
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
